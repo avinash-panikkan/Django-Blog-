113d15ed34715e045834cbbcf278b9b19c7bfcc9
Avinash Panikkan <avinkb11@gmail.com>
2023-04-21 00:13:29 +0530

    added admin, created databases, user registration, login, logout, profile

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-post = [
-    {
-        'author': 'Avi',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': '17 April 2023'
-    },
-    {
-        'author': 'Athi',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': '18 April 2023'
-    }
-]
 
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
